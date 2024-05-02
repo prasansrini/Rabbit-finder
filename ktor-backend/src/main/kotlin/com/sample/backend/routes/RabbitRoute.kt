2a153147d4473137045db0cab7c951b6759c7b9c
Prasanna <github@prasansrini.com>
2024-05-02 18:00:25 +0200

    Added pictures and updated the URL.

--- ktor-backend/src/main/kotlin/com/sample/backend/routes/RabbitRoute.kt
+++ ktor-backend/src/main/kotlin/com/sample/backend/routes/RabbitRoute.kt
@@ -8,12 +8,12 @@ import io.ktor.server.routing.*
 
 private const val BASE_URL: String = "http://192.168.1.180:8080"
 private val rabbits = listOf(
-	Rabbit("Prasanna", "New rabbit", ""),
-	Rabbit("Adi", "New rabbit", ""),
-	Rabbit("Shyam", "New rabbit", ""),
-	Rabbit("John", "New rabbit", ""),
-	Rabbit("Ram", "New rabbit", ""),
-	Rabbit("Moni", "New rabbit", "")
+	Rabbit("Prasanna", "New rabbit", "$BASE_URL/rabbits/rabbit1.jpg"),
+	Rabbit("Adi", "New rabbit", "$BASE_URL/rabbits/rabbit3.jpg"),
+	Rabbit("Shyam", "New rabbit", "$BASE_URL/rabbits/rabbit4.jpg"),
+	Rabbit("John", "New rabbit", "$BASE_URL/rabbits/rabbit5.jpg"),
+	Rabbit("Ram", "New rabbit", "$BASE_URL/rabbits/rabbit6.jpg"),
+	Rabbit("Moni", "New rabbit", "$BASE_URL/rabbits/rabbit1.jpg")
 )
 
 fun Route.randomRabbit() {
